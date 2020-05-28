7c902bd8480f9a6272e8f785d2c69c7101e446c3
prehani <prehani@wisc.edu>
2020-05-28 01:05:22 -0500

    changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,19 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import guzzle_sphinx_theme
 
+html_theme_path = guzzle_sphinx_theme.html_theme_path()
+html_theme = 'guzzle_sphinx_theme'
+
+# Register the theme as an extension to generate a sitemap.xml
+extensions.append("guzzle_sphinx_theme")
+
+# Guzzle theme options (see theme.conf for more information)
+html_theme_options = {
+    # Set the name of the project to appear in the sidebar
+    "project_nav_name": "Project Name",
+}
 
 # -- Project information -----------------------------------------------------
 
